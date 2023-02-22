2751d38431b62d0f9f329f7469b5d1d52d0c5275
Nikita <116362682+Nottezz@users.noreply.github.com>
2023-02-22 21:01:19 +0300

    update and fixed bug
    Refactor the entire code and fixed some critical and minor bugs.

--- registration.py
+++ registration.py
@@ -12,122 +12,162 @@ cur.execute("""CREATE TABLE IF NOT EXISTS users_data(
     Code INTEGER NOT NULL);
 """)
 db.commit()
+try:   # При отмене действия будет выводиться соответствующее сообщение
 
-# """Заполнение таблицы"""
-#
-# user_1 = ('Ivan', 'qwer1234', '1234')
-# cur.execute("""INSERT INTO users_data(Login, Password, Code)
-#     VALUES(?, ?, ?);
-# """, user_1)
-# db.commit()
+    """"Выбор действия"""
 
-"""Выбор действия"""
+    create_user = '1'  # Создать новый аккаунт
+    enter = '2'  # Авторизоваться
+    recover_password = '3'  # Восстановить пароль
+    action = input('Выберите цифру какую операцию выполнить?\n1 - Создать новый аккаунт\n2 - Авторизоваться\n3 - '
+                   'Восстановить пароль\n')
 
-create_user = '1'  # Создать новый аккаунт
-enter = '2'  # Авторизоваться
-recover_password = '3'  # Восстановить пароль
-action = input('Что нужно выполнить?\n1 - Создать новый аккаунт\n2 - Авторизоваться\n3 - Восстановить пароль\n')
+    if len(action) == 0 or action >= str(4) or action == str(0):
+        print('Неизвестная команда')
 
-"""Создаём нового пользователя"""
-if action == create_user:
+    """Создаём нового пользователя"""
 
-    login = input('Введите свой логин: ')
+    while True:                                                     # Цикл, который помогает нам сделать так, что бы не было пустых строк.
+        if action == create_user:
 
-    cur.execute(f"""SELECT Login FROM users_data WHERE Login = '{login}';""")
-    date_login = cur.fetchone()
+            login = input('Введите свой логин: ')
+            if len(login) == 0:                                     # Конструкция, которая прекращает действие, если пользователь оставил пустую строку.
+                print('Ошибка! Повторите попытку.')
+                break
+            else:
+                cur.execute(f"""SELECT Login FROM users_data WHERE Login = '{login}';""")
+                date_login = cur.fetchone()
 
-    if date_login == None:
-        password = input('Введите пароль от аккаунта: ')
-        code = input('Введите восстанавливающий код: ')
-        cur.execute(f"""INSERT INTO users_data(Login, Password, Code)
-                    VALUES ('{login}', '{password}', {code});
-                    """)
-        db.commit()
-        print('Новый пользователь создан!')
-    else:
-        print('Такой пользователь существует')
+                if date_login is None:
+
+                    password = input('Введите пароль от аккаунта: ')
+                    if len(password) == 0:
+                        print('Ошибка! Повторите попытку.')
+                        break
+                    else:
+                        code = input('Введите восстанавливающий код: ')
+                        if len(code) == 0:
+                            print('Ошибка! Повторите попытку.')
+                            break
+                        else:
+                            cur.execute(f"""INSERT INTO users_data(Login, Password, Code)
+                                        VALUES ('{login}', '{password}', {code});
+                                        """)
+                            db.commit()
+                            print('\nНовый пользователь создан!')
+                else:
+                    print('\nТакой пользователь существует')
+        break
 
     """Авторизация существующего пользователя"""
-elif action == enter:
-    login = input('Введите свой логин: ')
-    password = input('Введите пароль от аккаунта: ')
-    user = login + ", " + password
 
-    cur.execute("""SELECT * FROM users_data;""")
-    users_data = cur.fetchone()
-    data_login = users_data[1]
-    data_password = users_data[2]
-    authorization = data_login + ', ' + data_password
-
-    if user == authorization:
-        print('Авторизация прошла успешно!')
-    else:
-        print('Неверный логин или пароль. Осталось попыток: 3')
-        login = input('Введите свой логин: ')
-        password = input('Введите пароль от аккаунта: ')
-        user = login + ", " + password
-
-        cur.execute("""SELECT * FROM users_data;""")
-        users_data = cur.fetchone()
-        data_login = users_data[1]
-        data_password = users_data[2]
-        authorization = data_login + ', ' + data_password
-
-        if user == authorization:
-            print('Авторизация прошла успешно!')
-
-        else:
-            print('Неверный логин или пароль. Осталось попыток: 2')
+    while True:
+        if action == enter:
             login = input('Введите свой логин: ')
-            password = input('Введите пароль от аккаунта: ')
-            user = login + ", " + password
-
-            cur.execute("""SELECT * FROM users_data;""")
-            users_data = cur.fetchone()
-            data_login = users_data[1]
-            data_password = users_data[2]
-            authorization = data_login + ', ' + data_password
-
-            if user == authorization:
-                print('Авторизация прошла успешно!')
-
+            if len(login) == 0:
+                print('Ошибка! Cтрочка не должна быть пустой.')
+                break
             else:
-                print('Неверный логин или пароль. Осталось попыток: 1')
-                login = input('Введите свой логин: ')
-                password = input('Введите пароль от аккаунта: ')
-                user = login + ", " + password
 
-                cur.execute("""SELECT * FROM users_data;""")
-                users_data = cur.fetchone()
-                data_login = users_data[1]
-                data_password = users_data[2]
-                authorization = data_login + ', ' + data_password
+                cur.execute(f"""SELECT Login FROM users_data WHERE Login = '{login}';""")
+                users_login = cur.fetchone()
 
-                if user == authorization:
-                    print('Авторизация прошла успешно!')
-
+                if users_login is None:
+                    print('\nТакого пользователя не существует!')
+                    break
                 else:
-                    print('Восстановите пароль от аккаунта, используя код!')
 
-    """Изменяем пароль через кодовое слово"""
+                    password = input('Введите пароль от аккаунта: ')
+                    if len(password) == 0:
+                        print('Ошибка! Cтрочка не должна быть пустой.')
+                        break
+                    else:
+                        cur.execute(f"""SELECT Password FROM users_data WHERE Password = '{password}';""")
+                        users_password = cur.fetchone()
+                        attempt = 3
 
-elif action == recover_password:
-    login = input('Введите свой логин: ')
-    recover_code = input('Введите кодовое слово: ')
-    log_user = login + ", " + recover_code
+                    if users_password is None:
+                        attempt -= 1  # Cлужит для отсчёта попыток
+                        print(f'Пароль не подходит. Осталось попыток: {attempt}')
 
-    cur.execute(f"""SELECT Login, Code FROM users_data WHERE Login = '{login}' AND Code = '{recover_code}';""")
-    user_data = cur.fetchone()
+                        password = input('Введите пароль от аккаунта: ')
+                        if len(password) == 0:
+                            print('Ошибка! Cтрочка не должна быть пустой.')
+                            break
+                        else:
+                            cur.execute(f"""SELECT Password FROM users_data WHERE Password = '{password}';""")
+                            users_password = cur.fetchone()
 
-    if user_data == None:
-        print('Вы вели неверные данные! Повторите попытку')
-    else:
+                            if users_password is None:
+                                attempt -= 1
+                                print(f'Пароль не подходит. Осталось попыток: {attempt}')
 
-        new_password = input('Введите новый пароль: ')
+                                password = input('Введите пароль от аккаунта: ')
+                                if len(password) == 0:
+                                    print('Ошибка! Cтрочка не должна быть пустой.')
+                                    break
+                                else:
+                                    cur.execute(f"""SELECT Password FROM users_data WHERE Password = '{password}';""")
+                                    users_password = cur.fetchone()
 
-        cur.execute(f"""UPDATE users_data SET Password = '{new_password}' WHERE Code = '{recover_code}' ;""")
-        db.commit()
-        print('Новый пароль установлен!')
+                                    if users_password is None:
+                                        attempt -= 1
+                                        print(f'Осталось попыток: {attempt}. Восстановите пароль, используя код!')
+                                        break
+                                    else:
+                                        print('\nАвторизацуия прошла успешно!')
+                            else:
+                                print('\nАвторизацуия прошла успешно!')
 
-else:
-    print('Неизвестная команда! Повторите попытку.')
+                    else:
+                        print('\nАвторизацуия прошла успешно!')
+
+        break
+
+    """Восстановление аккаунта"""
+    while True:
+        if action == recover_password:
+
+            login = input('Введите cвой логин: ')
+            if len(login) == 0:
+                print('Ошибка! Cтрочка не должна быть пустой.')
+                break
+            else:
+                cur.execute(f"""SELECT Login FROM users_data WHERE Login = '{login}';""")
+                users_login = cur.fetchone()
+
+                if users_login is None:
+                    print('\nТакого пользователя не существует!')
+                    break
+                else:
+
+                    recover_code = input('Введите восстанавливающий код: ')
+                    if len(recover_code) == 0:
+                        print('Ошибка! Cтрочка не должна быть пустой.')
+                        break
+                    else:
+                        cur.execute(f"""SELECT Code FROM users_data WHERE Login = '{login}';""")
+                        users_code = cur.fetchone()
+                        date_code = users_code[0]
+
+                        if str(date_code) != str(recover_code):
+                            print('Вы ввели неверный код! Повторите попытку')
+                            break
+                        else:
+
+                            new_password = input('Введите новый пароль, состоящий более чем из 3 символов: ')
+
+                            if len(new_password) == 0:
+                                print('Ошибка! Cтрочка не должна быть пустой.')
+                                break
+                            elif len(new_password) <= 3:
+                                print('Пароль должен состоять минимум из 4 символов')
+                                break
+                            else:
+                                cur.execute(f"""UPDATE users_data SET Password = '{new_password}' WHERE Login = '{login}'
+                                AND Code = '{recover_code}';""")
+                                db.commit()
+                                print('\nНовый пароль установлен!')
+        break
+except KeyboardInterrupt:
+    print('\nОперация отменена!')
